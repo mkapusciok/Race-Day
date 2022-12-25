583dcd5839b5426dc84dd0fc6efaf3dfdc813421
mkapusciok <m.kapusciok@interia.pl>
2022-12-25 20:35:16 +0100

    Finished conditional statement

--- main.js
+++ main.js
@@ -2,5 +2,20 @@ let raceNumber = Math.floor(Math.random() * 1000);
 let registeredEarly = true;
 let runnerAge = 20;
 if (runnerAge >= 18 && registeredEarly === true) {
-    raceNumber += 1000;
+  raceNumber += 1000;
+}
+if (runnerAge > 18 && registeredEarly === true) {
+  console.log(
+    `Your race will start at 9:30AM! Your race number is ${raceNumber}.`
+  );
+} else if (runnerAge > 18 && registeredEarly === false) {
+  console.log(
+    `Your race will start at 11:00AM! Your race number is ${raceNumber}.`
+  );
+} else if (runnerAge < 18) {
+  console.log(
+    `Your race will start at 12:30PM! Your race number is ${raceNumber}.`
+  );
+} else {
+  console.log("Please contact registration!");
 }
